662269f63aaa7c22e03942ca281eef9e73553036
Reketino <bjornevensk8@gmail.com>
2025-10-19 11:04:58 +0200

    import os for å kjøre på render

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS  
 import requests, random
+import os
 
 app = Flask(__name__)
 CORS(app)  
@@ -22,4 +23,5 @@ def quote():
     return jsonify(fetch_quote())
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
