8835c781b1852666045293922319c0cb743c4754
Reketino <bjornevensk8@gmail.com>
2025-10-19 22:23:24 +0200

    oppdaterte språk menyen

--- frontend/app/components/Oversetter.jsx
+++ frontend/app/components/Oversetter.jsx
@@ -33,16 +33,32 @@ export default function Translator({ quote }) {
       <select
         value={language}
         onChange={(e) => setLanguage(e.target.value)}
-        className="bg-yellow-700 border px-2 py-1 rounded mr-2"
+        className="bg-black/70 text-yellow-200 border border-yellow-700 px-2 py-1 rounded-lg mr-2 shadow-lg backdrop-blur-sm focus:ring-2 focus:ring-yellow-500"
       >
-        <option value="no">Norwegian</option>
-        <option value="sv">Swedish</option>
-        <option value="fi">Finnish</option>
-        <option value="en">English</option>
-        <option value="ja">Japanese</option>
-        <option value="es">Spanish</option>
-        <option value="fr">French</option>
-        <option value="de">German</option>
+        <option className="bg-yellow-700 text-white" value="no">
+          Norwegian
+        </option>
+        <option className="bg-yellow-700 text-white" value="sv">
+          Swedish
+        </option>
+        <option className="bg-yellow-700 text-white" value="fi">
+          Finnish
+        </option>
+        <option className="bg-yellow-700 text-white" value="en">
+          English
+        </option>
+        <option className="bg-yellow-700 text-white" value="ja">
+          Japanese
+        </option>
+        <option className="bg-yellow-700 text-white" value="es">
+          Spanish
+        </option>
+        <option className="bg-yellow-700 text-white" value="fr">
+          French
+        </option>
+        <option className="bg-yellow-700 text-white" value="de">
+          German
+        </option>
       </select>
 
       <button
